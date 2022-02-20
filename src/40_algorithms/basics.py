0a8b94dc03a17d8079700fb3eba00f5db6934a40
Riyadh Uddin <noreplythnx@gmail.com>
2022-02-20 23:04:21 +0600

    m

---
+++ src/40_algorithms/basics.py
@@ -0,0 +1,10 @@
+def example_list():
+    pass
+def example_dict():
+    pass
+def example_tuple():
+    pass
+def example_sets():
+    pass
+def example_df():
+    pass
---
+++ src/algods/dynamic/lil_pony.py
@@ -0,0 +1,2 @@
+# A sequence of positive integers bi is harmony if and only if for every two elements of the sequence their greatest common divisor equals 1. According to an ancient book, the key of the chest is a harmony sequence bi which minimizes the following expression:
+#You are given sequence ai, help Princess Twilight to find the key.
